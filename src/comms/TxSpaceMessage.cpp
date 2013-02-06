a419cd50f87d07b940e51892b0aa03873c13a906
Maia Grotepass <maiatoday@gmail.com>
2013-02-06 14:19:54 +0200

    Incomplete - task : Make JSON formatted version of packets jcon object version to encode kinect info subset

--- src/comms/TxSpaceMessage.cpp
+++ src/comms/TxSpaceMessage.cpp
@@ -23,12 +23,37 @@ void TxSpaceMessage::makeHelloResponse() {
 	msgIdString = MSG_ID_HELLO_STRING;
 	setMsgIdFromString();
 	root = json_pack("{s:s}", MSGID_KEY, msgIdString.c_str());
+
 }
 
 void TxSpaceMessage::makeSpaceInfoResponse(vector<ofPoint>& userCenters, ofColor foreground) {
 	msgIdString = MSG_ID_SPACE_INFO_STRING;
 	setMsgIdFromString();
-	root = json_pack("{s:s}", MSGID_KEY, msgIdString.c_str());
+	json_t* users = json_array();
+	for (unsigned int i = 0; i < userCenters.size(); i++) {
+
+		json_t* c = json_object();
+		json_object_set_new(c, "x", json_integer(userCenters[i].x));
+		json_object_set_new(c, "y", json_integer(userCenters[i].y));
+		json_object_set_new(c, "z", json_integer(userCenters[i].z));
+
+
+		json_t* u = json_object();
+		json_object_set_new(u, "center", c);
+
+		json_array_append_new(users, u);
+
+	}
+	json_t* color = json_object();
+	json_object_set_new(color, "r", json_integer(foreground.r));
+
+	root = json_object();
+	json_object_set_new(root, MSGID_KEY, json_string(msgIdString.c_str()));
+	json_object_set_new(root, "fgColor", color);
+	json_object_set_new(root, "users", users);
+//	root = json_pack("{s:s}", MSGID_KEY, msgIdString.c_str());
+
+
 }
 
 void TxSpaceMessage::makeSpaceInfoAllResponse() {
